f2975954c50b1bc84bb2489780f5f4c49aa167f0
Pawan Jain <jainpawan211199@gmail.com>
2024-08-22 14:28:08 +0530

    fix: deploy refactor

--- src/app/components/layout/AppContainer.tsx
+++ src/app/components/layout/AppContainer.tsx
@@ -1,18 +1,39 @@
+"use client";
 import { Box, Flex } from "@chakra-ui/react";
 import Navbar from "../Navbar";
 import SendNotificationForm from "../SendNotificationForm";
 import NotificationFeed from "../NotificationFeed";
+import { useEffect, useState } from "react";
+import { createId } from "@paralleldrive/cuid2";
 
 const AppContainer = () => {
-    return (
-        <Box height="100vh" bg="gray.100">
-            <Navbar />
-            <Flex height="calc(100vh - 72px)" padding={8} alignItems="flex-start" gap={6}>
-                <SendNotificationForm />
-                <NotificationFeed />
-            </Flex>
-        </Box>
-    );
+  const [subscriberId, setSubscriberId] = useState("");
+
+  useEffect(() => {
+    const localSubscriberId = localStorage.getItem("inbox_demo_subscriberId");
+
+    if (localSubscriberId) {
+      setSubscriberId(localSubscriberId);
+    } else {
+      const newSubscriberId = createId();
+      localStorage.setItem("inbox_demo_subscriberId", newSubscriberId);
+      setSubscriberId(newSubscriberId);
+    }
+  }, []);
+  return (
+    <Box height="100vh" bg="gray.100">
+      <Navbar />
+      <Flex
+        height="calc(100vh - 72px)"
+        padding={8}
+        alignItems="flex-start"
+        gap={6}
+      >
+        <SendNotificationForm subscriberId={subscriberId} />
+        <NotificationFeed subscriberId={subscriberId} />
+      </Flex>
+    </Box>
+  );
 };
 
 export default AppContainer;
