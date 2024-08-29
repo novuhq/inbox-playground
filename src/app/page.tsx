bf72d50ebad009f4e4fbf0ca7c77d24bbafdba09
Dima Grossman <dima@grossman.io>
2024-08-29 13:17:39 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import AppContainer from './components/layout/AppContainer';
-
+"use client";
+import { ChakraProvider } from "@chakra-ui/react";
+import AppContainer from "./components/layout/AppContainer";
 
 export default function Home() {
   return (
