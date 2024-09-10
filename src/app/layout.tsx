da206d333e07fa308b18a618cb848a466c18cbbd
Dima Grossman <dima@grossman.io>
2024-09-10 16:36:03 +0300

    fix: add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Inbox Component Playground | Novu",
-  description: "Example application to show in-app notifications powered by Novu",
+  description:
+    "Example application to show in-app notifications powered by Novu",
   icons: {
     icon: "/novuIcon.svg", // Path to your favicon
   },
@@ -19,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-KXMC4XP2" />
       <body className={inter.className}>{children}</body>
     </html>
   );
