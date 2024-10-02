efb5e3f786c8e699efd0688407dd951176881324
Thi Van Le <vannnyle@gmail.com>
2024-10-02 14:56:56 +0200

    chore: hover state for button

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -53,6 +53,10 @@ const theme = extendTheme({
           bg: 'linear-gradient(90deg, #DE2574, #FD4F32)',
           color: 'white',
           borderRadius: '11px',
+          transition: 'background 0.2s',
+          _hover: {
+            bg: 'linear-gradient(90deg, rgba(222, 37, 116, 0.80) 0%, rgba(253, 79, 50, 0.80) 100%)',
+          },
         },
       },
     },
