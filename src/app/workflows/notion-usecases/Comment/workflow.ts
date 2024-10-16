fa7849a0771536ca138cb9b190828e5197127079
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2024-10-16 20:25:30 +0300

    testing notion-comment workflow by passing controls again

--- src/app/workflows/notion-usecases/Comment/workflow.ts
+++ src/app/workflows/notion-usecases/Comment/workflow.ts
@@ -1,5 +1,5 @@
 import { workflow } from "@novu/framework";
-import { payloadSchema } from "./payloadSchema";
+import { z } from "zod";
 
 const workflowName = "notion-comment-notification";
 
@@ -11,35 +11,59 @@ export const notionCommentNotification = workflow(
     // -----------------------------------in-app step-------------------------------------------------------------------------
     await step.inApp(
       "in-app-step",
-      async () => {
+      async (controls) => {
         const result: any = {
-          subject: payload.inAppSubject,
-          body: payload.inAppBody,
+          subject: controls.inAppSubject,
+          body: controls.inAppBody,
         };
 
-        if (payload.showInAppAvatar) {
-          result.avatar = payload.inAppAvatar;
+        if (controls.showInAppAvatar) {
+          result.avatar = controls.inAppAvatar;
         }
 
-        if (payload.enablePrimaryAction) {
+        if (controls.enablePrimaryAction) {
           result.primaryAction = {
-            label: payload.inAppPrimaryActionLabel,
-            url: payload.inAppPrimaryActionUrl,
+            label: controls.inAppPrimaryActionLabel,
+            url: controls.inAppPrimaryActionUrl,
           };
         }
 
-        if (payload.enableSecondaryAction) {
+        if (controls.enableSecondaryAction) {
           result.secondaryAction = {
-            label: payload.inAppSecondaryActionLabel,
-            url: payload.inAppSecondaryActionUrl,
+            label: controls.inAppSecondaryActionLabel,
+            url: controls.inAppSecondaryActionUrl,
           };
         }
         return result;
       },
+      {
+        controlSchema: z.object({
+subscriberFirstName: z.string().default("John"),
+          subscriberLastName: z.string().default("Doe"),
+          inAppSubject: z
+            .string()
+            .default(
+              `${subscriber?.firstName} ${subscriber?.lastName} commented in`
+            ),
+          inAppBody: z.string().default("Important Page"),
+          inAppAvatar: z
+            .string()
+            .default("https://avatars.githubusercontent.com/u/63902456?v=4"),
+          showInAppAvatar: z.boolean().default(true),
+          inAppPrimaryActionLabel: z.string().default("Reply"),
+          enablePrimaryAction: z.boolean().default(true),
+          inAppPrimaryActionUrl: z.string().default("https://novu.com"),
+          inAppSecondaryActionLabel: z.string().default("Dismiss"),
+          enableSecondaryAction: z.boolean().default(false),
+          inAppSecondaryActionUrl: z.string().default("https://novu.com"),
+        }),
+      }
     );
+
+    // -----------------------------------payload schema-------------------------------------------------------------------------
   },
   {
+    // -----------------------------------tags-------------------------------------------------------------------------
     tags: ["Comment"],
-    payloadSchema: payloadSchema
   }
 );
