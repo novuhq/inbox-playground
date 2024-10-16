13562812440b407c23b454ff06c8c333b9e7a302
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2024-10-16 21:12:35 +0300

    testing another fix

--- src/app/workflows/notion-usecases/Comment/stepsControlSchema.ts
+++ src/app/workflows/notion-usecases/Comment/stepsControlSchema.ts
@@ -91,3 +91,18 @@ export const digestControlSchema = z.object({
 
 export const customControlSchema = z.object({
 });
+
+export const payloadSchema = z.object({
+  firstName: z.string().default("John"), 
+  lastName: z.string().default("Doe"),
+  inAppSubject: z.string().default("In-App Notification Subject!"),
+  inAppBody: z.string().default("In-App Notification Body!"),
+  inAppAvatar: z.string().default("https://avatars.githubusercontent.com/u/63902456?v=4"),
+  showInAppAvatar: z.boolean().default(true),
+  inAppPrimaryActionLabel: z.string().default("Reply"),
+  enablePrimaryAction: z.boolean().default(true),
+  inAppPrimaryActionUrl: z.string().default("https://novu.com"),
+  inAppSecondaryActionLabel: z.string().default("Dismiss"),
+  enableSecondaryAction: z.boolean().default(false),
+  inAppSecondaryActionUrl: z.string().default("https://novu.com"),
+});
--- src/app/workflows/notion-usecases/Comment/workflow.ts
+++ src/app/workflows/notion-usecases/Comment/workflow.ts
@@ -1,5 +1,6 @@
 import { workflow } from "@novu/framework";
-import { z } from "zod";
+import { inAppControlSchema } from "./stepsControlSchema";
+import { payloadSchema } from "./payloadSchema";
 
 const workflowName = "notion-comment-notification";
 
@@ -7,8 +8,6 @@ const workflowName = "notion-comment-notification";
 export const notionCommentNotification = workflow(
   workflowName,
   async ({ step, payload, subscriber }) => {
-    // Define the step for the workflow
-    // -----------------------------------in-app step-------------------------------------------------------------------------
     await step.inApp(
       "in-app-step",
       async (controls) => {
@@ -37,33 +36,12 @@ export const notionCommentNotification = workflow(
         return result;
       },
       {
-        controlSchema: z.object({
-          subscriberFirstName: z.string().default("John"),
-          subscriberLastName: z.string().default("Doe"),
-          inAppSubject: z
-            .string()
-            .default(
-              `${subscriber?.firstName} ${subscriber?.lastName} commented in`
-            ),
-          inAppBody: z.string().default("Important Page"),
-          inAppAvatar: z
-            .string()
-            .default("https://avatars.githubusercontent.com/u/63902456?v=4"),
-          showInAppAvatar: z.boolean().default(true),
-          inAppPrimaryActionLabel: z.string().default("Reply"),
-          enablePrimaryAction: z.boolean().default(true),
-          inAppPrimaryActionUrl: z.string().default("https://novu.com"),
-          inAppSecondaryActionLabel: z.string().default("Dismiss"),
-          enableSecondaryAction: z.boolean().default(false),
-          inAppSecondaryActionUrl: z.string().default("https://novu.com"),
-        }),
+        controlSchema: inAppControlSchema,
       }
     );
-
-    // -----------------------------------payload schema-------------------------------------------------------------------------
   },
   {
-    // -----------------------------------tags-------------------------------------------------------------------------
+    payloadSchema: payloadSchema,
     tags: ["Comment"],
   }
 );
